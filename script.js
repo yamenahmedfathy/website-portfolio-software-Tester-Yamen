8903e6d3df8cdd112fbc4f0c788e7feb9ede9ed1
Yamen Ahmed <yamenaahmedfathy@gmail.com>
2025-09-08 00:58:35 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-=======
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
->>>>>>> 6cafbfa (First commit)
+
